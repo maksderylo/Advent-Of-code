34066d52ac66e03eea2e477f3e1f843e2e12fc37
maksderylo <129659049+maksderylo@users.noreply.github.com>
2023-06-01 13:09:35 +0200

    day 1

--- Day 1/day1part2.cpp
+++ Day 1/day1part2.cpp
@@ -1,25 +1,29 @@
 #include<fstream>
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
+
+bool mniej(int a, int b){
+    return a > b;
+}
 
 int main(){
     ifstream in;
     in.open("input.txt");
     string liczba;
-    int sum=0,maxsum=0;
+    int sum=0;
+    vector<int>v;
     while(getline(in, liczba)){
         if(liczba==""){
+            v.push_back(sum);
             sum=0;
         }
         else{
             sum+= stoi(liczba);
         }
-
-        if(sum>maxsum){
-            maxsum=sum;
-        }
-
     }
-    cout<<maxsum;
-    
+    sort(v.begin(),v.end(), mniej);
+    cout<<v[0]+v[1]+v[2];
+
 }
